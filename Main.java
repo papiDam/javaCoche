548906afe5f12d430eb05eac9c0f8ed40be1ba23
papiDam <unho1991@gmail.com>
2022-03-11 12:32:27 +0100

    con coche

--- Main.java
+++ Main.java
@@ -4,13 +4,14 @@ class Main{
       Motor miMotor = new Motor();
       Puerta miPuerta = new Puerta();
       Puerta miPuerta2 = new Puerta();
-
+      Coche miCoche = new Coche();
+/*
       miMotor.encender();
       miPuerta.getVentanaPuerta().abrir();
       miPuerta2.abrir();
 
 
-/*
+
       System.out.println(miPuerta);
       miPuerta.abrir();
       System.out.println(miPuerta);
@@ -18,12 +19,13 @@ class Main{
       System.out.println(miPuerta);
       miPuerta.cerrar();
       System.out.println(miPuerta);
-*/
+
       System.out.println(miMotor);
       System.out.println(miPuerta);
       System.out.println(miPuerta2);
+*/
 
-
+      System.out.println(miCoche);
 
 
 
