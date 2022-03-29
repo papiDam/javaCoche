7a49bdb2fc77d31bb6e908657581b55d48962f0e
papiDam <unho1991@gmail.com>
2022-03-29 12:25:42 +0200

    Fin herencia persona

--- java/Clase persona/Main.java
+++ java/Clase persona/Main.java
@@ -10,10 +10,17 @@ class Main {
     System.out.println(miAlumno);
     System.out.println(miProfesor);
 
+    ArrayList<Integer> lista = new ArrayList();
+    lista.add(4);
+    lista.add(5);
+    System.out.println(lista);
+
     ArrayList<Alumno> listaAlumnos = new ArrayList();
     listaAlumnos.add(miAlumno);
     listaAlumnos.add(alumnoDefecto);
     System.out.println(listaAlumnos);
     System.out.println(listaAlumnos.get(0).getNombre());
+
+    System.out.println(miAlumno.getNombre());
   }
 }
